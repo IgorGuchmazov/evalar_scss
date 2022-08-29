c9542039c746ff59609d409a59d8210134ac110b
Igor <igor.guchmazovwork@gmail.com>
2022-08-29 17:55:45 +0300

    add scss evalar

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        appendArrows:$('.arrows')
+    });
+});
+
+$(document).ready(function(){
+    $('.slider-second').slick({
+        appendArrows:$('.arrows-second')
+    });
+});
